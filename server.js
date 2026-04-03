cea4b6ee8919b868c873f4b63db702e8e29c8114
Tseprosper1234 <tseprosper02@gmail.com>
2026-04-03 11:50:33 +0100

    Update backend files and configuration

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.use(cookieParser());
 
 // CORS configuration
 app.use(cors({
-  origin:  'http://localhost:5173',
+  origin: 'https://cajst.catuc.org',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
